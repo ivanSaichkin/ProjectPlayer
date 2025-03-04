7829ca3291f387787cd4687b6bd13762678e1bab
Andrey Pavlinich <pavlinitohaaa@gmail.com>
2025-03-04 21:56:09 +0300

    add Button_lib ver 04.03

---
+++ VideoPlayer/Button/Button.hpp
@@ -0,0 +1,27 @@
+#ifndef BUTTON_HPP
+#define BUTTON_HPP
+
+#include <SFML/Graphics.hpp>
+#include <functional>
+
+class Button {
+public:
+    Button(const std::string& label, const sf::Font& font, const sf::Vector2f& position, const sf::Vector2f& size);
+
+    void draw(sf::RenderWindow& window) const;
+    bool isMouseOver(const sf::RenderWindow& window) const;
+    void onClick(std::function<void()> callback);
+
+    void setLabel(const std::string& label);
+    void setPosition(const sf::Vector2f& position);
+    void setSize(const sf::Vector2f& size);
+
+private:
+    sf::RectangleShape shape;
+    sf::Text labelText;
+    std::function<void()> onClickCallback;
+
+    void updateTextPosition();
+};
+
+#endif // BUTTON_HPP
